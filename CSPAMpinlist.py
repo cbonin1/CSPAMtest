c21c040fb34b4dfdb40ca10fd9a72714d5250c38
cbonin1 <141946837+cbonin1@users.noreply.github.com>
2023-08-10 09:57:43 -0700

    Add files via upload

---
+++ CSPAMpinlist.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# this file contains a list of RPi GPIO pins used by BIU
+cannon = 26  # Sprayer
+plunger = 13  # Plunge control
+dht22 = 24  # Temperature and humidity monitor - currently not used
+filterposition = 19  # Piezo control
+sensorpower = 6  # Not used --- always powered up
+pedalsensor = 20  #
+interlock = 21  #
+led = 18  # thorlabs led lamp
+pwm0 = 26 # this is is the io pin number of the chip - not the rPi gpio number... Used by LED
+
